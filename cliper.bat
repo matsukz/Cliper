1db8b5c1b2114b845472b3701a871c4707ac18bd
まつ <92625267+matsukz@users.noreply.github.com>
2022-07-14 10:05:29 +0900

    2022-07-14

--- cliper.bat
+++ cliper.bat
@@ -1,5 +1,6 @@
 @echo off
 
+
 :start
 echo =======================
 echo コピーするものを選択…
@@ -13,31 +14,25 @@ echo ========================
 SET COPY=
 SET /P COPY="対応した項目の入力："
 
-if "%COPY%"=="" goto start
-if %COPY%==0 goto end
+if "%COPY%"=="" goto :start
+if %COPY%==0 goto :end
 
 if %COPY%==1 SET FILE=ID
 if %COPY%==2 SET FILE=pass
 If %COPY%==3 SET FILE=mail
-if %COPY% geq 4 SET FILE="###"
+if %COPY% geq 4 SET FILE=###
 
 
 echo %FILE%
 if %COPY%==### (
-    goto false
-) else (
-    echo LSS4
-    clip < %FILE%.clip
-    echo %errorlevel%
-    echo OK
-    echo.
-    goto start
-)
-
-:false
     echo 0から3の値を入力してください。
     echo.
-    goto start
+    goto :start
+    ) else (
+        clip < %FILE%.clip
+        echo.
+        goto :start
+        )
 
 
 :end
