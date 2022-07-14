7028046a8bdff8bcee1c758cb7ec63d2139d5a1a
まつ <92625267+matsukz@users.noreply.github.com>
2022-07-14 10:41:04 +0900

    2022-07-14

--- cliper.bat
+++ cliper.bat
@@ -1,39 +1,38 @@
 @echo off
 
-
 :start
-echo =======================
-echo コピーするものを選択…
-echo.
-echo (1)ID
-echo (2)パスワード
-echo (3)メールアドレス
-echo (0)プログラム終了
-echo ========================
+    SET COPY="###"
+    echo =======================
+    echo コピーするものを選択…
+    echo.
+    echo (1)ID
+    echo (2)パスワード
+    echo (3)メールアドレス
+    echo (0)プログラム終了
+    echo ========================
 
-SET COPY=
-SET /P COPY="対応した項目の入力："
+    SET COPY=
+    SET /P COPY="対応した項目の入力："
 
 if "%COPY%"=="" goto :start
 if %COPY%==0 goto :end
+if %COPY% geq 4 goto false
 
-if %COPY%==1 SET FILE=ID
-if %COPY%==2 SET FILE=pass
-If %COPY%==3 SET FILE=mail
-if %COPY% geq 4 SET FILE=###
-
-
-echo %FILE%
-if %COPY%==### (
-    echo 0から3の値を入力してください。
+:true
+    if %COPY%==1 SET FILE=ID
+    if %COPY%==2 SET FILE=pass
+    If %COPY%==3 SET FILE=mail
+    echo %FILE%
+    clip < %FILE%.clip
+    echo OK
     echo.
-    goto :start
-    ) else (
-        clip < %FILE%.clip
-        echo.
-        goto :start
-        )
+    goto start
 
+:false
+    echo %FILE%
+    echo 入力された値が不正です。
+    echo.
+    goto start
 
 :end
     echo.
